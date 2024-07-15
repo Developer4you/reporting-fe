21d0838fb7c05dffd53bf9fe08b6288511e85aa9
andrei andrei <andrei@joyspay.com>
2024-07-15 22:26:14 +0300

    update files

--- src/http/index.ts
+++ src/http/index.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import {AuthResponse} from "../models/response/AuthRethponse";
 
-export const API_URL = 'https://developer4you-forgias-srv-4ed8.twc1.net/api'
+export const API_URL = process.env.REACT_APP_API_URL
 
 const $api = axios.create({
     withCredentials:true,
